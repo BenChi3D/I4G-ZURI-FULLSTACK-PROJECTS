77efbe67185c8a22a84c72a5164c2815aab50b71
BenChi3D <benediktchima@gmail.com>
2022-05-18 13:35:28 +0100

    counting words

---
+++ Counting-Words/README.md
@@ -0,0 +1,3 @@
+# Task Title: Counting Words
+
+Write a python program that counts and returns the number of words in a given text.
---
+++ Counting-Words/count-word.py
@@ -0,0 +1,8 @@
+def count(word):
+    counted = len(word)
+    print(counted)
+
+# calling the function 
+count("I4GZURI") # example 1
+count("Scholarship") # example 2
+count("Task created by ANDE") # example 3
