76a10f177d32c1d06aa28a210b9606a3ef0cd37b
BenChi3D <benediktchima@gmail.com>
2022-05-29 19:01:23 +0100

    classes and objects

--- Classes-and-objects/main.py
+++ Classes-and-objects/main.py
@@ -1,29 +1,42 @@
 class Student:
+    """
+    This is a Student class
+    """
     # [assignment] Skeleton class. Add your code here
     def __init__(self, name, age, tracks, score):
         self.name = name
         self.age = age
         self.tracks = tracks
-        self.score =score
+        self.score = score
+        pass
 
-    def change_name(self, name):
-        self.change_name = name
+    def change_name(self, change_name):
+        self.change_name = change_name
+        pass
 
-    def change_age(self, age):
-        self.change_age = age
+    def change_age(self, change_age):
+        self.change_age = change_age
+        pass
 
+    def add_track(self, add_track):
+        self.add_track = add_track
+        pass
+
+    def get_score(self, get_score):
+        self.get_score = get_score
         pass
 
 
+Bob = Student("Bob", 26, ["FE", "BE"], 20.90)
+Bob = Student("Peter", 34, "UI/UX", " ")
 
-Bob = Student("Bob", 26, ["FE","BE"],20.90)
-Bob = Student("Peter", 34, "UI/UX", ())
 # Expected methods
-#Bob.change_name("Peter")
-#Bob.change_age(34)
-#Bob.add_track("UI/UX")
-#Bob.get_score()
+Bob.change_name("John")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score(75)  # inputting a score
 
 print(Bob.change_name)
-print(Bob.age)
-print(Bob.change_name)
+print(Bob.change_age)
+print(Bob.add_track)
+print(Bob.get_score)
