a98a697446e65ef0fbe7654112400031c1d8875a
BenChi3D <benediktchima@gmail.com>
2022-06-21 19:26:41 +0100

    control flow addition

--- CLI Python Calc/CLIcalc.py
+++ CLI Python Calc/CLIcalc.py
@@ -6,28 +6,29 @@ def calculator():
     * for multiplication
     / for division
     ''')
-
-    # numbers to be worked with by the user
-    num1 = int(input('Enter your first number:\n'))
-    num2 = int(input('Enter your second number:\n'))
+    if operation == '+' or operation == '-' or operation == '*' or operation == '/':    
+        # numbers to be worked with by the user
+        num1 = int(input('Enter your first number:\n'))
+        num2 = int(input('Enter your second number:\n'))
     
-    if operation == '+' :
-        # Addition
-        print('{} + {} = '.format(num1 , num2))
-        print(num1 + num2)
-    elif operation == '-' :
-        # Subtraction
-        print('{} - {} = '.format(num1 , num2))
-        print(num1 - num2)
-    elif operation == '*':
-        # Multiplication
-        print('{} * {} = '.format(num1 , num2))
-        print(num1 * num2)
-    elif operation == '/':
-        # division
-        print('{} / {} = '.format(num1 , num2))
-        print(num1 / num2)
+        if operation == '+' :
+            # Addition
+            print('{} + {} = '.format(num1 , num2))
+            print(num1 + num2)
+        elif operation == '-' :
+            # Subtraction
+            print('{} - {} = '.format(num1 , num2))
+            print(num1 - num2)
+        elif operation == '*':
+            # Multiplication
+            print('{} * {} = '.format(num1 , num2))
+            print(num1 * num2)
+        elif operation == '/':
+            # division
+            print('{} / {} = '.format(num1 , num2))
+            print(num1 / num2)
     else:
         print('What you entered is not a valid operator')
+        calculator()
 
 calculator()
