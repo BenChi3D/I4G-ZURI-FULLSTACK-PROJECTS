ac17f79d75c3b44f5b5ad6e14e8b29d88ad0025c
BenChi3D <benediktchima@gmail.com>
2022-06-24 01:36:15 +0100

    print to console

---
+++ intro-to-javascript/README.md
@@ -0,0 +1,8 @@
+# Task Title: Introduction to Javascript
+
+Write a javascript code that prints out
+- your name
+- your height
+- your country 
+
+on the screen
---
+++ intro-to-javascript/print.js
@@ -0,0 +1,7 @@
+let name = "Benedict Chima Ogbulachi"
+let height = 1.76
+let country = "Nigeria"
+
+console.log(name)
+console.log(height)
+console.log(country)
